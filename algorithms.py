fbe57577b3c707fd59a2087a3d9a98333e168bd9
michal.koziarski <michal.koziarski@gmail.com>
2020-09-21 16:36:54 +0200

    simplified algorithm

--- algorithms.py
+++ algorithms.py
@@ -19,26 +19,31 @@ def potential(x, points, gamma):
 
 
 def normalize(v):
-    n = torch.norm(v, p=2).detach()
-
-    return v.div(n)
+    return v.div(torch.norm(v, p=2).detach())
 
 
-def loss_function(anchors, prototypes, points, gamma):
-    real_potentials = torch.zeros(anchors.shape[0])
-    prototype_potentials = torch.zeros(anchors.shape[0])
+def normalized_potential(anchors, points, gamma):
+    result = torch.zeros(anchors.shape[0])
 
     for i, anchor in enumerate(anchors):
-        real_potentials[i] = potential(anchor, points, gamma)
-        prototype_potentials[i] = potential(anchor, prototypes, gamma)
+        result[i] = potential(anchor, points, gamma)
 
-    return ((normalize(real_potentials) - normalize(prototype_potentials)) ** 2).mean()
+    result = normalize(result)
+
+    return result
 
 
-class PAO:
-    def __init__(self, gamma=0.25, n_anchors=25, learning_rate=10.0, iterations=500,
+def loss_function(anchors, prototypes, gamma, reference_potential):
+    return ((reference_potential - normalized_potential(anchors, prototypes, gamma)) ** 2).mean()
+
+
+class AbstractPA:
+    def __init__(self, kind, gamma=0.25, n_anchors=25, learning_rate=0.001, iterations=200,
                  tolerance=1e-6, epsilon=1e-4, minority_class=None, n=None, seed=None,
                  device=torch.device('cpu')):
+        assert kind in ['oversample', 'undersample']
+
+        self.kind = kind
         self.gamma = gamma
         self.n_anchors = n_anchors
         self.learning_rate = learning_rate
@@ -92,24 +97,27 @@ class PAO:
             device=self.device, requires_grad=False, dtype=torch.float
         )
 
+        if self.kind == 'oversample':
+            reference_points = minority_points
+        else:
+            reference_points = majority_points
+
         self._prototypes = torch.tensor(
-            minority_points[np.random.randint(minority_points.shape[0], size=n), :] +
-            np.random.normal(scale=self.epsilon, size=(n, minority_points.shape[1])),
+            reference_points[np.random.randint(reference_points.shape[0], size=n), :] +
+            np.random.normal(scale=self.epsilon, size=(n, reference_points.shape[1])),
             device=self.device, requires_grad=True, dtype=torch.float
         )
 
-        minority_points = torch.tensor(
-            minority_points,
-            device=self.device, requires_grad=False, dtype=torch.float
-        )
+        reference_points = torch.tensor(reference_points, device=self.device, requires_grad=False, dtype=torch.float)
+        reference_potential = normalized_potential(self._anchors, reference_points, self.gamma)
 
-        optimizer = torch.optim.SGD([self._prototypes], lr=self.learning_rate)
+        optimizer = torch.optim.Adam([self._prototypes], lr=self.learning_rate)
 
         self._loss = []
 
         with tqdm(total=self.iterations) as pbar:
             for i in range(self.iterations):
-                loss = loss_function(self._anchors, self._prototypes, minority_points, self.gamma)
+                loss = loss_function(self._anchors, self._prototypes, self.gamma, reference_potential)
 
                 if self.tolerance is not None and len(self._loss) > 1 and self._loss[-2] - self._loss[-1] < self.tolerance:
                     break
@@ -128,4 +136,37 @@ class PAO:
         self._anchors = self._anchors.cpu().detach().numpy()
         self._prototypes = self._prototypes.cpu().detach().numpy()
 
-        return np.concatenate([X, self._prototypes]), np.concatenate([y, minority_class * np.ones(n)])
+        if self.kind == 'oversample':
+            X_ = np.concatenate([X, self._prototypes])
+            y_ = np.concatenate([y, minority_class * np.ones(n)])
+        else:
+            X_ = np.concatenate([minority_points, self._prototypes])
+            y_ = np.concatenate([minority_class * np.ones(len(minority_points)), majority_class * np.ones(n)])
+
+        return X_, y_
+
+
+class PAO(AbstractPA):
+    def __init__(self, gamma=0.25, n_anchors=25, learning_rate=0.001, iterations=200,
+                 tolerance=1e-6, epsilon=1e-4, minority_class=None, n=None, seed=None,
+                 device=torch.device('cpu')):
+        super().__init__(
+            kind='oversample', gamma=gamma, n_anchors=n_anchors,
+            learning_rate=learning_rate, iterations=iterations,
+            tolerance=tolerance, epsilon=epsilon,
+            minority_class=minority_class,
+            n=n, seed=seed, device=device
+        )
+
+
+class PAU(AbstractPA):
+    def __init__(self, gamma=0.25, n_anchors=25, learning_rate=0.001, iterations=200,
+                 tolerance=1e-6, epsilon=1e-4, minority_class=None, n=None, seed=None,
+                 device=torch.device('cpu')):
+        super().__init__(
+            kind='undersample', gamma=gamma, n_anchors=n_anchors,
+            learning_rate=learning_rate, iterations=iterations,
+            tolerance=tolerance, epsilon=epsilon,
+            minority_class=minority_class,
+            n=n, seed=seed, device=device
+        )
