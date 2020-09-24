1b8074f1554b192ce7d6a39fd1896cacf47e9f53
michal.koziarski <michal.koziarski@gmail.com>
2020-09-24 19:48:33 +0200

    added preliminary gamma analysis

---
+++ schedule_preliminary_gamma.py
@@ -0,0 +1,9 @@
+import os
+
+
+if __name__ == '__main__':
+    for fold in range(10):
+        for classifier_name in ['KNN', 'CART', 'L-SVM', 'R-SVM', 'P-SVM', 'LR', 'R-MLP']:
+            command = f'sbatch run.sh run_preliminary_gamma.py -fold {fold} -classifier_name {classifier_name}'
+
+            os.system(command)
