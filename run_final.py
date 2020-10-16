76b612e427ab695ad2e46a7004f076a685469b11
michal.koziarski <michal.koziarski@gmail.com>
2020-10-16 15:12:10 +0200

    simplified final experiment; added CPA

--- run_final.py
+++ run_final.py
@@ -6,7 +6,7 @@ import numpy as np
 import pandas as pd
 import smote_variants as sv
 
-from algorithms import PAO
+from algorithms import CPA, PAO, PAU
 from pathlib import Path
 from sklearn.linear_model import LogisticRegression
 from sklearn.naive_bayes import GaussianNB
@@ -16,58 +16,63 @@ from sklearn.svm import LinearSVC, SVC
 from sklearn.tree import DecisionTreeClassifier
 
 
-def evaluate_trial(classifier_name, fold):
+def evaluate_trial(resampler_name, fold):
     RESULTS_PATH = Path(__file__).parents[0] / 'results_final'
     RANDOM_STATE = 42
 
     resamplers = {
-        'SMOTE': sv.distance_SMOTE,
-        'polynom-fit-SMOTE': sv.polynom_fit_SMOTE,
-        'ProWSyn': sv.ProWSyn,
-        'SMOTE-IPF': sv.SMOTE_IPF,
-        'Lee': sv.Lee,
-        'SMOBD': sv.SMOBD,
-        'G-SMOTE': sv.G_SMOTE,
-        'CCR': sv.CCR,
-        'LVQ-SMOTE': sv.LVQ_SMOTE,
-        'Assembled-SMOTE': sv.Assembled_SMOTE,
-        'SMOTE-TomekLinks': sv.SMOTE_TomekLinks,
-        'PAO': PAO
+        'SMOTE': sv.distance_SMOTE(random_state=RANDOM_STATE),
+        'polynom-fit-SMOTE': sv.polynom_fit_SMOTE(random_state=RANDOM_STATE),
+        'ProWSyn': sv.ProWSyn(random_state=RANDOM_STATE),
+        'SMOTE-IPF': sv.SMOTE_IPF(random_state=RANDOM_STATE),
+        'Lee': sv.Lee(random_state=RANDOM_STATE),
+        'SMOBD': sv.SMOBD(random_state=RANDOM_STATE),
+        'G-SMOTE': sv.G_SMOTE(random_state=RANDOM_STATE),
+        'CCR': sv.CCR(random_state=RANDOM_STATE),
+        'LVQ-SMOTE': sv.LVQ_SMOTE(random_state=RANDOM_STATE),
+        'Assembled-SMOTE': sv.Assembled_SMOTE(random_state=RANDOM_STATE),
+        'SMOTE-TomekLinks': sv.SMOTE_TomekLinks(random_state=RANDOM_STATE),
+        'PAO': PAO(random_state=RANDOM_STATE),
+        'PAU': PAU(random_state=RANDOM_STATE),
+        'CPA.1': CPA(ratio=0.1, random_state=RANDOM_STATE),
+        'CPA.9': CPA(ratio=0.9, random_state=RANDOM_STATE)
     }
 
     for dataset_name in datasets.names():
-        for resampler_name in resamplers.keys():
-            trial_name = f'{dataset_name}_{fold}_{classifier_name}_{resampler_name}'
-            trial_path = RESULTS_PATH / f'{trial_name}.csv'
+        classifiers = {
+            'CART': DecisionTreeClassifier(random_state=RANDOM_STATE),
+            'KNN': KNeighborsClassifier(n_neighbors=3),
+            'L-SVM': LinearSVC(random_state=RANDOM_STATE),
+            'R-SVM': SVC(random_state=RANDOM_STATE, kernel='rbf'),
+            'P-SVM': SVC(random_state=RANDOM_STATE, kernel='poly'),
+            'LR': LogisticRegression(random_state=RANDOM_STATE),
+            'NB': GaussianNB(),
+            'R-MLP': MLPClassifier(random_state=RANDOM_STATE),
+            'L-MLP': MLPClassifier(random_state=RANDOM_STATE, activation='identity')
+        }
 
-            if trial_path.exists():
-                continue
+        trial_name = f'{dataset_name}_{fold}_{resampler_name}'
+        trial_path = RESULTS_PATH / f'{trial_name}.csv'
 
-            logging.info(f'Evaluating {trial_name}...')
+        if trial_path.exists():
+            continue
 
-            dataset = datasets.load(dataset_name)
+        logging.info(f'Evaluating {trial_name}...')
 
-            (X_train, y_train), (X_test, y_test) = dataset[fold][0], dataset[fold][1]
+        dataset = datasets.load(dataset_name)
 
-            classifiers = {
-                'CART': DecisionTreeClassifier(random_state=RANDOM_STATE),
-                'KNN': KNeighborsClassifier(n_neighbors=3),
-                'L-SVM': LinearSVC(random_state=RANDOM_STATE),
-                'R-SVM': SVC(random_state=RANDOM_STATE, kernel='rbf'),
-                'P-SVM': SVC(random_state=RANDOM_STATE, kernel='poly'),
-                'LR': LogisticRegression(random_state=RANDOM_STATE),
-                'NB': GaussianNB(),
-                'R-MLP': MLPClassifier(random_state=RANDOM_STATE),
-                'L-MLP': MLPClassifier(random_state=RANDOM_STATE, activation='identity')
-            }
+        (X_train, y_train), (X_test, y_test) = dataset[fold][0], dataset[fold][1]
 
+        resampler = resamplers[resampler_name]
+
+        assert len(np.unique(y_train)) == len(np.unique(y_test)) == 2
+
+        X_train, y_train = resampler.sample(X_train, y_train)
+
+        rows = []
+
+        for classifier_name in classifiers.keys():
             classifier = classifiers[classifier_name]
-
-            resampler = resamplers[resampler_name](random_state=RANDOM_STATE)
-
-            assert len(np.unique(y_train)) == len(np.unique(y_test)) == 2
-
-            X_train, y_train = resampler.sample(X_train, y_train)
 
             clf = classifier.fit(X_train, y_train)
             predictions = clf.predict(X_test)
@@ -81,18 +86,16 @@ def evaluate_trial(classifier_name, fold):
                 'F-measure': metrics.f_measure
             }
 
-            rows = []
-
             for scoring_function_name in scoring_functions.keys():
                 score = scoring_functions[scoring_function_name](y_test, predictions)
                 row = [dataset_name, fold, classifier_name, resampler_name, scoring_function_name, score]
                 rows.append(row)
 
-            columns = ['Dataset', 'Fold', 'Classifier', 'Resampler', 'Metric', 'Score']
+        columns = ['Dataset', 'Fold', 'Classifier', 'Resampler', 'Metric', 'Score']
 
-            RESULTS_PATH.mkdir(exist_ok=True, parents=True)
+        RESULTS_PATH.mkdir(exist_ok=True, parents=True)
 
-            pd.DataFrame(rows, columns=columns).to_csv(trial_path, index=False)
+        pd.DataFrame(rows, columns=columns).to_csv(trial_path, index=False)
 
 
 if __name__ == '__main__':
@@ -100,9 +103,9 @@ if __name__ == '__main__':
 
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('-classifier_name', type=str)
     parser.add_argument('-fold', type=int)
+    parser.add_argument('-resampler_name', type=str)
 
     args = parser.parse_args()
 
-    evaluate_trial(args.classifier_name, args.fold)
+    evaluate_trial(args.resampler_name, args.fold)
--- schedule_final.py
+++ schedule_final.py
@@ -1,9 +1,18 @@
 import os
 
 
+RESAMPLERS = [
+    'SMOTE', 'polynom-fit-SMOTE', 'ProWSyn',
+    'SMOTE-IPF', 'Lee', 'SMOBD', 'G-SMOTE',
+    'CCR', 'LVQ-SMOTE', 'Assembled-SMOTE',
+    'SMOTE-TomekLinks', 'PAO', 'PAU',
+    'CPA.1', 'CPA.9'
+]
+
+
 if __name__ == '__main__':
     for fold in range(10):
-        for classifier_name in ['KNN', 'CART', 'L-SVM', 'R-SVM', 'P-SVM', 'LR', 'R-MLP']:
-            command = f'sbatch run.sh run_final.py -fold {fold} -classifier_name {classifier_name}'
+        for resampler_name in RESAMPLERS:
+            command = f'sbatch run.sh run_final.py -fold {fold} -resampler_name {resampler_name}'
 
             os.system(command)
