f22ca14be76fdf5c2f6e58f372471354245162f0
michal.koziarski <michal.koziarski@gmail.com>
2020-11-28 20:05:51 +0100

    added preliminary analysis script

---
+++ analyse_preliminary.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+from pathlib import Path
+
+
+VISUALIZATIONS_PATH = Path(__file__).parent / 'visualizations'
+
+
+if __name__ == '__main__':
+    VISUALIZATIONS_PATH.mkdir(exist_ok=True, parents=True)
+
+    df = pd.read_csv(f'results/results_preliminary.csv')
+
+    g = sns.catplot(
+        data=df, x='Ratio', y='Score', row='Classifier', col='Metric',
+        sharey='col', height=2.0, aspect=1.4, kind='point', ci=None,
+        row_order=['KNN', 'CART', 'SVM', 'MLP'],
+        col_order=['Precision', 'Recall', 'AUC', 'G-mean'],
+        hue='Metric'
+    )
+    g.set_titles('{row_name}, {col_name}')
+    g.set_xticklabels(rotation=45)
+
+    for ax in g.axes.flat:
+        ax.tick_params(axis='y', labelleft=True)
+
+    plt.savefig(VISUALIZATIONS_PATH / f'preliminary_ratio.pdf', bbox_inches='tight')
