321a7a272e864cb37f3c3665750f9ea987799255
michal.koziarski <michal.koziarski@gmail.com>
2020-11-28 17:27:41 +0100

    deleted experiments

--- schedule_preliminary_gamma.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-if __name__ == '__main__':
-    for fold in range(10):
-        for gamma in [0.25, 0.5, 0.75, 1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0]:
-            command = f'sbatch run.sh run_preliminary_gamma.py -fold {fold} -gamma {gamma}'
-
-            os.system(command)
--- schedule_preliminary_n_anchors.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-if __name__ == '__main__':
-    for fold in range(10):
-        for n_anchors in [3, 6, 9, 12, 15, 18, 21, 24, 27, 30]:
-            command = f'sbatch run.sh run_preliminary_n_anchors.py -fold {fold} -n_anchors {n_anchors}'
-
-            os.system(command)
