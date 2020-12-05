a0591e8eaf39a5682563743c0373418942d2a159
michal.koziarski <michal.koziarski@gmail.com>
2020-12-05 17:40:38 +0100

    merge results script

---
+++ merge_results.py
@@ -0,0 +1,23 @@
+import pandas as pd
+
+from pathlib import Path
+from tqdm import tqdm
+
+
+RESULTS_PATH = Path(__file__).parent / 'results'
+
+
+if __name__ == '__main__':
+    RESULTS_PATH.mkdir(exist_ok=True, parents=True)
+
+    for trial in ['pa_rb_comparison', 'ratio', 'final', 'noise_level']:
+        input_directory = Path(f'results_{trial}')
+
+        dfs = []
+
+        for path in tqdm(input_directory.iterdir(), desc=f'Trial: {trial}'):
+            df = pd.read_csv(path)
+            dfs.append(df)
+
+        df = pd.concat(dfs)
+        df.to_csv(RESULTS_PATH / f'results_{trial}.csv', index=False)
