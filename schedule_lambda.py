4b23391996bf70dcb7ace017f5c1dcdb719de5e0
michal.koziarski <michal.koziarski@gmail.com>
2020-12-06 19:47:35 +0100

    additional lambda values

--- schedule_lambda.py
+++ schedule_lambda.py
@@ -3,7 +3,7 @@ import os
 
 if __name__ == '__main__':
     for fold in range(10):
-        for lambd in [0.0, 0.00001, 0.0001, 0.001, 0.01, 0.1]:
+        for lambd in [0.0, 0.00001, 0.0001, 0.001, 0.01, 0.1, 1.0, 10.0]:
             command = f'sbatch run.sh run_lambda.py -fold {fold} -lambd {lambd}'
 
             os.system(command)
