f7786e6d584431c0544c544ae0c8f7da962c113c
michal.koziarski <michal.koziarski@gmail.com>
2020-12-10 18:10:06 +0100

    added visualization of example datasets

---
+++ visualize_pau.py
@@ -0,0 +1,32 @@
+from algorithms import *
+from datasets import names
+from visualization import *
+
+
+if __name__ == '__main__':
+    for dataset_name in names():
+        gamma = 0.1
+
+        X, y = prepare_data(dataset_name, scaler='MinMax', n_minority_samples=None)
+        minority_class = Counter(y).most_common()[1][0]
+        pau = PAU(gamma=gamma, random_state=42)
+        X_, y_ = pau.sample(X, y)
+
+        visualize(
+            X, y, gamma=gamma,
+            anchors=pau._anchors,
+            potential_type='majority',
+            file_name=f'example_pau_{dataset_name}_original',
+            minority_class=minority_class,
+            lim=(-0.05, 1.05)
+        )
+
+        visualize(
+            X_, y_, gamma=gamma,
+            replaced=pau._prototypes,
+            anchors=pau._anchors,
+            potential_type='majority',
+            file_name=f'example_pau_{dataset_name}_resampled',
+            minority_class=minority_class,
+            lim=(-0.05, 1.05)
+        )
