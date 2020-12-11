a7392cf7758d300097f7ad80f8fdd01f0f5d0caf
michal.koziarski <michal.koziarski@gmail.com>
2020-12-11 13:33:15 +0100

    anchors removed from visualization

--- visualize_lambda.py
+++ visualize_lambda.py
@@ -9,18 +9,16 @@ if __name__ == '__main__':
     X, y = prepare_data(dataset_name, scaler='Standard', n_minority_samples=None)
     minority_class = Counter(y).most_common()[1][0]
 
+    visualize(
+        X, y, gamma=gamma,
+        potential_type='minority',
+        file_name=f'example_pao_{dataset_name}_original',
+        minority_class=minority_class
+    )
+
     for lambd in [0.0, 0.001, 10.0]:
         pao = PAO(gamma=gamma, lambd=lambd, random_state=42)
         pao.sample(X, y)
-
-        if lambd == 0.0:
-            visualize(
-                X, y, gamma=gamma,
-                anchors=pao._anchors,
-                potential_type='minority',
-                file_name=f'example_pao_{dataset_name}_original',
-                minority_class=minority_class
-            )
 
         visualize(
             X, y, gamma=gamma,
